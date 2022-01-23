030b388c77ebce09a58f42ac85c101c6009545ad
Sam Foster <samueljamesfoster@gmail.com>
2022-01-23 16:28:53 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'warhammer',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
